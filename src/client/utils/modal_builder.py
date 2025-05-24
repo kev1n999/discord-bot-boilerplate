5056eb3c43ca12837bcc7a7eb464233c2b34dde7
kev1n999 <sopratesta999@gmail.com>
2025-05-24 16:44:04 -0300

    modal builder updates

--- src/client/utils/modal_builder.py
+++ src/client/utils/modal_builder.py
@@ -1,22 +1,37 @@
 import discord 
-from typing import List 
+from typing import List, Coroutine
 
 class InputTextBuilder(discord.ui.TextInput):
-    def __init__(self, label: str=None, style:str | discord.TextStyle=None, custom_id: str | None=None):
+    def __init__(self, label: str=None, placeholder: str=None, style:str | discord.TextStyle=None, custom_id: str | None=None, required: bool=None):
+        self.text_styles = {
+            "short": discord.TextStyle.short ,
+            "long": discord.TextStyle.long,
+            "paragraph": discord.TextStyle.paragraph
+        }
+        
         if label is None:
             raise TypeError("Erro: Label não informado na criação do InputText!")
         
         if style is None:
-            style = discord.TextStyle.short
+            style = self.text_styles["short"]
+        else:
+            style = self.text_styles[style.lower()]
+        
+        if required is None:
+            required = True 
             
         super().__init__(
             label=label,
             style=style,
-            custom_id=custom_id
+            placeholder=placeholder,
+            custom_id=custom_id, 
+            required=required
         )
         
 class ModalBuilder(discord.ui.Modal):
-    def __init__(self, title: str=None, custom_id: str=None, persistent: bool=None, *, items: List[InputTextBuilder] | discord.TextInput=None):
+    def __init__(self, title: str=None, custom_id: str=None, persistent: bool=None, *, items: List[InputTextBuilder] | discord.TextInput=None, modal_listener: Coroutine):
+        self.modal_listener = modal_listener
+        
         if items is None:
             raise TypeError("Erro: Nenhum item foi passado!")
         
@@ -31,4 +46,6 @@ class ModalBuilder(discord.ui.Modal):
                 self.add_item(item)
         else:
             self.add_item(items)
-
+    
+    async def on_submit(self, interaction: discord.Interaction):
+        await self.modal_listener(interaction)
--- src/commands/examples/ping.py
+++ src/commands/examples/ping.py
@@ -1,5 +1,6 @@
 import discord 
 from src.client.utils.command_builder import SlashCommandBuilder
+from src.client.utils.modal_builder import InputTextBuilder, ModalBuilder
 
 # Simple example for creating commands for the bot
 class ExampleCommandPing(SlashCommandBuilder):
@@ -12,4 +13,11 @@ class ExampleCommandPing(SlashCommandBuilder):
         
     # Command callback
     async def callback(self, interaction: discord.Interaction):
-        await interaction.response.send_message("Pong!")
+        i = InputTextBuilder(label="oi", style="long", placeholder="test placeholder", custom_id="a")
+        
+        modal = ModalBuilder(title="modal1", items=[i], custom_id="a", modal_listener=self.modal_listener)
+        
+        await interaction.response.send_modal(modal)
+        
+    async def modal_listener(self, interaction):
+        await interaction.response.send_message("pong")
